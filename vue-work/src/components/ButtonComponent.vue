df8ebb4862b8282012b9d63de7acfd3e56ed11cd
drsabaumar <sabarashid195@gmail.com>
2022-03-17 16:47:24 +0800

    Vue.js first push

---
+++ vue-work/src/components/ButtonComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <button @click="onClick()" :style="{background:color}" class="btn">{{text}}</button>
+</template>
+<script>
+export default {
+    name: 'ButtonComponent',
+    props: {
+        text: String,
+        color: String,
+    },
+    methods: {
+        onclick() {
+            console.log('click')
+        },
+    },
+}
+</script>
---
+++ vue-work/src/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+import Button from './ButtonComponent'
+
+export default {
+    name:'HeaderComponent',
+    props: {
+        title: String,
+    },
+    components: {
+        Button,
+    }
+}
+</script>
+
+<style scoped>
+header{
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+}
+    
+</style>
